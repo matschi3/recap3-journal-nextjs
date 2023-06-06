94ac6efd0289727ff2df227fa345d6daa033f05f
Nils Fischer <nils@designatic.de>
2023-06-06 21:37:12 +0200

    structured with containers

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
 import Header from "@/components/Header";
 import Container from "@/components/Container";
+import useLocalStorageState from "use-local-storage-state";
 
 export default function Home() {
+  const [entries, setEntries] = useLocalStorageState("personalEntries", {
+    defaultValue: {
+      id: "00000000",
+      title: "Beispiel-Titel",
+      text: "Hier könnte dein Beitrag stehen. Trage schnell etwas ins Formular ein um zu starten!",
+      isFavorite: false,
+    },
+  });
+
   return (
     <>
       <Header title="JOURNAL in Next.js" />
       <Container direction="column" align="center">
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
+        <p>form</p>
+        <Container direction="column" align="center">
+          <p>tabBar</p>
+          <Container direction="column" align="center">
+            <p>entries</p>
+          </Container>
+        </Container>
       </Container>
     </>
   );
