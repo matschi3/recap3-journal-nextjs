43f6d8b7c9dd98af704cf8d11cd38c963d36bbc6
Nils Fischer <nils@designatic.de>
2023-06-07 09:14:53 +0200

    planned entries

--- components/Entries.js
+++ components/Entries.js
@@ -1,3 +1,16 @@
-export default function Entries() {
-  return;
+import Container from "./Container";
+
+export default function Entries({ entries }) {
+  return (
+    <Container direction="column" align="center">
+      {entries.map((entry) => {
+        <section key={entry.id}>
+          <p>{entry.date}</p>
+          <p>{entry.title}</p>
+          <p>{entry.text}</p>
+          <button>SVG</button>
+        </section>;
+      })}
+    </Container>
+  );
 }
