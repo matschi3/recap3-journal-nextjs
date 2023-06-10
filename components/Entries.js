a613fd02630d9b3e961981b5f1ce68801ec51304
Nils Fischer <nils@designatic.de>
2023-06-10 14:11:13 +0200

    add delButton

--- components/Entries.js
+++ components/Entries.js
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 import Container from "./Container";
 import ButtonBookmark from "./ButtonBookmark";
+import ButtonDelete from "./ButtonDelete";
 
 const StyledArticle = styled.article`
   position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
-export default function Entries({ entries, handleBookmark }) {
+export default function Entries({ entries, handleBookmark, handleDelete }) {
   return (
     <Container direction="column" align="center">
       {entries.map((entry) => (
@@ -19,6 +23,7 @@ export default function Entries({ entries, handleBookmark }) {
             id={entry.id}
             handleBookmark={handleBookmark}
           />
+          <ButtonDelete entry={entry} handleDelete={handleDelete} />
         </StyledArticle>
       ))}
     </Container>
