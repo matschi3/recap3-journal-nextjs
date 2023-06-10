81282b66ef83569d81b7d948b0bc9f663af099eb
Nils Fischer <nils@designatic.de>
2023-06-10 10:55:26 +0200

    add entries

--- components/Entries.js
+++ components/Entries.js
@@ -1,16 +1,25 @@
 import Container from "./Container";
 
 export default function Entries({ entries }) {
+  console.log(entries);
   return (
     <Container direction="column" align="center">
-      {entries.map((entry) => {
-        <section key={entry.id}>
+      {entries.map((entry) => (
+        <article key={entry.id}>
           <p>{entry.date}</p>
           <p>{entry.title}</p>
           <p>{entry.text}</p>
           <button>SVG</button>
-        </section>;
-      })}
+        </article>
+      ))}
     </Container>
   );
 }
+
+/*  {
+  id: "00000000",
+  title: "Beispiel-Titel",
+  text: "Hier könnte dein Beitrag stehen. Trage schnell etwas ins Formular ein um zu starten!",
+  date: "10.06.2023",
+  isFavorite: false,
+} */
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
 import Header from "@/components/Header";
 import Container from "@/components/Container";
 import useLocalStorageState from "use-local-storage-state";
+import Entries from "@/components/Entries";
 
 export default function Home() {
   const [entries, setEntries] = useLocalStorageState("personalEntries", {
-    defaultValue: {
-      id: "00000000",
-      title: "Beispiel-Titel",
-      text: "Hier könnte dein Beitrag stehen. Trage schnell etwas ins Formular ein um zu starten!",
-      isFavorite: false,
-    },
+    defaultValue: [
+      {
+        id: "00000000",
+        title: "Beispiel-Titel",
+        text: "Hier könnte dein Beitrag stehen. Trage schnell etwas ins Formular ein um zu starten!",
+        date: "09.06.2023",
+        isFavorite: false,
+      },
+      {
+        id: "00000001",
+        title: "Beispiel-Titel #2",
+        text: "Hier könnte dein Beitrag stehen. Trage schnell etwas ins Formular ein um zu starten!",
+        date: "10.06.2023",
+        isFavorite: false,
+      },
+    ],
   });
 
   return (
@@ -20,7 +31,7 @@ export default function Home() {
         <Container direction="column" align="center">
           <p>tabBar</p>
           <Container direction="column" align="center">
-            <p>entries</p>
+            <Entries entries={entries} />
           </Container>
         </Container>
       </Container>
