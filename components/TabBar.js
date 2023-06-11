7b5a3bf348197cb06ec044ba198c94d457321ec9
Nils Fischer <nils@designatic.de>
2023-06-11 14:22:33 +0200

    try for category mapping, maybe duplicates

---
+++ components/TabBar.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function TabBar({ entries }) {
+  const categories = new Set(entries.map((entry) => entry.category));
+
+  return (
+    <nav>
+      {Array.from(categories).map((category) => (
+        <Link key={category} href={`/category/${category.categorySlug}`}>
+          <a>{category}</a>
+        </Link>
+      ))}
+    </nav>
+  );
+}
